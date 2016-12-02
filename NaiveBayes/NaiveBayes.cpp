1388bfe2a703908028ec80ae0df569ef9ce7980d
xbjiang <jxavierphysics@163.com>
2016-12-02 20:59:18 +0800

    add member function for training Naive Bayes

--- NaiveBayes/NaiveBayes.cpp
+++ NaiveBayes/NaiveBayes.cpp
@@ -1,7 +1,13 @@
+/*
+* Naive Bayes Classifier
+* p(ci|W) = p(W|ci)p(ci) = p(W0|ci)p(W1|ci)...p(Wn|ci)p(ci)
+*/
+
 #include <iostream>
 #include <map>
 #include <vector>
 #include <string>
+#include <numeric>
 using namespace std;
 
 string postings[6][10] = {
@@ -13,14 +19,18 @@ string postings[6][10] = {
 	{ "quit", "buying", "worthless", "dog", "food", "stupid", "null" }
 };
 
-int class_vec[6] = {0, 1, 0, 1, 0, 1};
+int class_vec[6] = {0, 1, 0, 1, 0, 1}; // 1 abusive 0 not 
 
 class NaiveBayes {
     private:
-		vector< vector<string> > posting_list;
-		vector<int> class_list;
-		map<string, int> vocab_list;
-		int* return_vec;
+		vector< vector<string> > posting_list;	// training set, each row for one record
+		vector<int> class_list;					// label of abusive or not
+		map<string, int> vocab_list;			// vocabulary set -> index
+		int* return_vec;						// words vector, return value of words2vec(int idx)
+		vector< vector<int> > train_mat;		// words matrix, each row for one record
+		vector<float> p_vec_c0;					// p(W|c0) likelihood:¡¡frequency vector for each word in non-abusive records
+		vector<float> p_vec_c1;					// p(W|c1) likelihood:¡¡frequency vector for each word in abusive records
+		float p_c1;								// p(c1) prior: frequency for abusive records
 
     public:
 		NaiveBayes()
@@ -78,12 +88,73 @@ class NaiveBayes {
 			}
 		}
 
+		void create_train_mat()
+		{
+			train_mat.clear();
+			for (int i = 0; i < class_list.size(); i++)
+			{
+				words2vec(i);
+				train_mat.push_back(vector<int>(return_vec, return_vec + vocab_list.size() + 1));
+			}
+		}
+
 		void print()
 		{
-			cout << "word vec: ";
-			for (int i = 0; i < vocab_list.size() + 1; i++)
-				cout << return_vec[i] << " ";
+			cout << "train matrix: " << endl;
+			for (int i = 0; i < class_list.size(); i++)
+			{
+				for (int j = 0; j < vocab_list.size() + 1; j++)
+					cout << train_mat[i][j] << " ";
+				cout << endl;
+			}
+			cout << "words frequency vector: " << endl;
+			cout << "p(Wi|c0): ";
+			for (int i = 0; i < p_vec_c0.size(); i++)
+				cout << p_vec_c0[i] << " ";
 			cout << endl;
+			cout << "p(Wi|c1): ";
+			for (int i = 0; i < p_vec_c1.size(); i++)
+				cout << p_vec_c1[i] << " ";
+			cout << endl;
+			cout << accumulate(p_vec_c0.begin(), p_vec_c0.end(), 0.0) << endl;
+			cout << accumulate(p_vec_c1.begin(), p_vec_c1.end(), 0.0) << endl;
+		}
+
+		void trainNB()
+		{
+			int num_records = class_list.size();
+			int num_abusive = accumulate(class_list.begin(), class_list.end(), 0);
+			p_c1 = (float)num_abusive / (float)num_records;
+
+			p_vec_c0.resize(vocab_list.size() + 1, 0); // words frequency vector
+			p_vec_c1.resize(vocab_list.size() + 1, 0);
+			float total_num_c0 = 0.0; // total numbers of words in non-abusive records
+			float total_num_c1 = 0.0;
+			for (int i = 0; i < class_list.size(); i++)
+			{
+				if (class_list[i] == 0)
+				{
+					for (int j = 0; j < train_mat[i].size(); j++)
+					{
+						p_vec_c0[j] += train_mat[i][j];
+						total_num_c0 += train_mat[i][j];
+					}
+				}
+				else
+				{
+					for (int j = 0; j < train_mat[i].size(); j++)
+					{
+						p_vec_c1[j] += train_mat[i][j];
+						total_num_c1 += train_mat[i][j];
+					}
+				}
+			}
+
+			for (int i = 0; i < p_vec_c0.size(); i++)
+			{
+				p_vec_c0[i] /= total_num_c0;
+				p_vec_c1[i] /= total_num_c1;
+			}
 		}
 
 		~NaiveBayes()
@@ -96,8 +167,7 @@ int main()
 {
 	NaiveBayes nb;
 	nb.create_vocab_list();
-	nb.words2vec(2);
-	nb.print();
-	nb.words2vec(3);
+	nb.create_train_mat();
+	nb.trainNB();
 	nb.print();
 }
