c7047f545e3a6833f82f4ca9efd9c09df72f33d9
xbjiang <jxavierphysics@163.com>
2016-12-04 18:24:04 +0800

    initCentroids

--- KMeans/kmeans.cpp
+++ KMeans/kmeans.cpp
@@ -12,7 +12,7 @@ class KMeans {
 private:
     vector< vector<T> > dataSet;
     int row, col;
-    vector<T> centeroids;
+    vector<T> centroids;
     int k;
     typedef struct MinMax {
         T Min;
@@ -29,9 +29,55 @@ private:
 public:
     KMeans(int k);
     void initCentroids();
+	void initRandCentroids();
     void loadDataSet();
     vector<int> genRandSeq(int bound);
-    int RandomInt(int bound);
+    int randomInt(int bound);
     tMinMax getMinMax(int idx);
     void kmeans();
+	T distEuclid(vector<T> vec1, vector<T> vec2)
 };
+
+template <typename T>
+KMeans<T>::KMeans(int k)
+{
+	this->k = k;
+}
+
+template <typename T>
+void KMeans<T>::initCentroids()
+{
+	vector<int> randSeq = genRandSeq(row);
+	for (int i = 0; i < K; i++)
+		centroids.push_back(dataSet[randSeq[i]]);
+}
+
+template <typename T>
+vector<int> KMeans<T>::genRandSeq(int bound)
+{
+	vector<int> nums;
+	// generate a sequence of 0,1,2,...,bound-1s
+	for (int i = 0; i < bound; i++) 
+		nums.push_back(i);
+	vector<int> ret;
+	int endIdx = row - 1;
+	for (int i = 0; i < k; i++)
+	{
+		int idx = randomInt(endIdx + 1);
+		ret.push_back(nums[idx]);
+		nums[idx] = nums[endIdx--];
+	}
+	return ret;
+}
+
+template <typename T>
+int KMeans<T>::randomInt(int bound)
+{
+	return rand() % bound;
+}
+
+template <typename T>
+void KMeans<T>::kmeans()
+{
+
+}
