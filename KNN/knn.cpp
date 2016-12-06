274115552bebd8115c5404ef5c5497ae2339ed3f
xbjiang <jxavierphysics@163.com>
2016-12-07 00:43:17 +0800

    KNN::classify

--- KNN/knn.cpp
+++ KNN/knn.cpp
@@ -8,6 +8,8 @@
 #include <fstream>
 #include <sstream>
 #include <stdlib.h>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 class KNN
@@ -23,6 +25,14 @@ public:
 	KNN(char* filename, int k, int dim);
 	double distEuclid(vector<double>& vec1, vector<double>& vec2);
 	void loadDataSet(char* filename, int dim);
+	void computeAllDist(vector<double>& testVec);
+	string classify(vector<double>& testVec);
+	struct cmpByValue {
+		bool operator() (pair<int, double>& lhs, pair<int, double>& rhs)
+		{
+			return lhs.second < rhs.second;
+		}
+	};
 };
 
 KNN::KNN(char* filename, int k, int dim) : K(K), col(dim)
@@ -56,4 +66,58 @@ void KNN::loadDataSet(char* filename, int dim)
 	}
 	row = labelSet.size();
 	fin.close();
+}
+
+double KNN::distEuclid(vector<double>& vec1, vector<double>& vec2)
+{
+	if (vec1.size() != vec2.size())
+	{
+		cerr << "Size of the inputs has to be same!" << endl;
+		exit(1);
+	}
+	double sum = 0;
+	for (int i = 0; i < vec1.size(); i++)
+	{
+		sum += pow(vec1[i] - vec2[i], 2);
+	}
+	return sqrt(sum);
+}
+
+void KNN::computeAllDist(vector<double>& testVec)
+{
+	for (int i = 0; i < col; i++)
+	{
+		double dist = distEuclid(dataSet[i], testVec);
+		distVec.push_back(make_pair(i, dist));
+	}
+}
+
+string KNN::classify(vector<double>& testVec)
+{
+	if (testVec.size() != col)
+	{
+		cerr << "Wrong dimension!" << endl;
+		exit(1);
+	}
+	computeAllDist(testVec);
+	sort(distVec.begin(), distVec.end(), cmpByValue());
+	map<string, int> labelCnt;
+	for (int i = 0; i < K; i++)
+	{
+		int idx = distVec[i].first;
+		labelCnt[labelSet[idx]]++;
+	}
+	int maxCnt = 0;
+	string majorLabel = "";
+	map<string, int>::iterator iter = labelCnt.begin();
+	while (iter != labelCnt.end())
+	{
+		if (iter->second > maxCnt)
+		{
+			maxCnt = iter->second;
+			majorLabel = iter->first;
+		}
+		iter++;
+	}
+	return majorLabel;
 }
