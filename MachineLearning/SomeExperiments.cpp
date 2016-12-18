b4f2cec3b5c3b9f13838ad5c7b37e6f3f02764df
xbjiang <jxavierphysics@163.com>
2016-12-18 12:29:43 +0800

    SomeExperiments

---
+++ MachineLearning/SomeExperiments.cpp
@@ -0,0 +1,12 @@
+class GamePlayer
+{
+private:
+    static const int NumTurns = 5;
+    int scores[NumTurns];
+};
+
+//const int GamePlayer::NumTurns;
+
+int main()
+{
+}
--- MachineLearning/test.cpp
+++ MachineLearning/test.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <stdlib.h>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    int rand_start = -1;
-    for (int j = 0; j < 10; j++)
-    {
-        rand_start = rand() % 10;
-        for (int i = rand_start; i < 10 + rand_start; i++)
-            cout << i % 10 << " ";
-        cout << endl;
-    }
-    
-}
+//int main()
+//{
+//    /*int rand_start = -1;
+//    for (int j = 0; j < 10; j++)
+//    {
+//        rand_start = rand() % 10;
+//        for (int i = rand_start; i < 10 + rand_start; i++)
+//            cout << i % 10 << " ";
+//        cout << endl;
+//    }*/
+//    /*vector<int> vec = {0,1,2,3,4};
+//    do {
+//        for (auto val : vec)
+//            cout << val << " ";
+//        cout << endl;
+//    }
+//    while (next_permutation(vec.begin(), vec.end()));*/
+//}
