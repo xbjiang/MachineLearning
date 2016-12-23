7fa763c21dcabeeabc8112093c1309f57baa91f7
xbjiang <jxavierphysics@163.com>
2016-12-23 22:12:58 +0800

    bug fix

--- AdaBoost/WeakLearner.h
+++ AdaBoost/WeakLearner.h
@@ -9,9 +9,13 @@ class WeakLearner
 {
 public:
 	WeakLearner() {};
-	virtual ~WeakLearner() = 0;
-	virtual int train(const fmatrix& X, const std::vector<float>& D) = 0;
-	virtual int classify(const std::vector<float> x) = 0; 
+	// virtual ~WeakLearner() = 0; 
+    // You don't need to declare destructor to be pure virtual here, because you have other pure virtual functions.
+    // If you did, you need to provide a definition for it because it will always be called after the destruction of the derived part.
+    ~WeakLearner() {};
+    virtual int train(const fmatrix& X, const std::vector<float>& Y, const std::vector<float>& D) = 0;
+	virtual float classify(const std::vector<float>& x) = 0; 
+    virtual const std::vector<float>& get_y_pred() = 0;
 };
 
 #endif
