21596a7b27243dadbca22f3fed6a38a64ce3d10b
xbjiang <jxavierphysics@163.com>
2016-12-23 12:40:41 +0800

    minor changes

--- MachineLearning/SomeExperiments.cpp
+++ MachineLearning/SomeExperiments.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 class GamePlayer
 {
 private:
@@ -7,6 +10,14 @@ private:
 
 //const int GamePlayer::NumTurns;
 
+char* p = "Hello World1";
+char* p1 = "Hello World1";
+char c[] = "Hello World2";
+
 int main()
 {
+    //p[1] = 'E';
+    //c[1] = 'E';
+    printf("%d\n", p);
+    printf("%d\n", p1);
 }
